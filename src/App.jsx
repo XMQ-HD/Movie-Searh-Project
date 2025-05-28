7c07e3e0bdaa45e4642896be55e2beb832580926
XMQ-HD <161677628+XMQ-HD@users.noreply.github.com>
2025-05-28 18:41:33 +0800

    Initial project to finish project

---
+++ src/App.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Search from './components/search'
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
+import { useDebounce }  from 'react-use';
+import { updateSearchCount } from './appwritejs';
+import { getTrendingMovies } from './appwritejs';
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+  const [searchItem, setSearchItem] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([])
+  const [isLoading, setIsloading] = useState(false)
+  const [deboundedSearchItem, setDebouncedSearchItem] = useState('')
+  
+  // Debounce the search input to avoid too many API calls
+  // This will wait for 500ms after the user stops typing before updating the debounced value
+  useDebounce(() => setDebouncedSearchItem(searchItem), 500, [searchItem]);
+
+  const fetchMovies = async (query='') => {
+    setIsloading(true)
+    setErrorMessage('')
+
+    try {
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies')
+      }
+
+      const  data = await response.json()
+
+      if(data.Respnse === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies')
+        setMovieList([])
+        return
+      }
+
+      setMovieList(data.results || [])
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
+
+    } catch (error) {
+      console.error('Error fetching movies:', error)
+      setErrorMessage('Failed to fetch movies. Please try again later.')
+    } finally {
+      setIsloading(false)
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies()
+
+      setTrendingMovies(movies)
+
+    } catch (error) {
+      console.error('Error fetching trending movies:', error)
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(deboundedSearchItem)
+
+  },[deboundedSearchItem])
+  
+  useEffect(() => {
+    loadTrendingMovies()
+  },[])
+
+  return (
+    <main>
+      <div className='pattern'/>
+      
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='hero' className='hero' />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchItem={searchItem} setSearchItem={setSearchItem}/>
+        </header>
+
+        { trendingMovies.length > 0 && (
+          <section className='trending'>
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title}/>
+                </li>
+              ))}
+            </ul>
+
+          </section>
+        )}
+
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+
+          {isLoading?(
+            <Spinner/>
+          
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ): (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )}
+
+        </section>
+
+      
+        
+      </div>
+      
+    </main>
+  )
+}
+
+export default App
+
